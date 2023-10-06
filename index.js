998c0293951c5c778245142396b4d546d7af8da1
Dylan Larimer <dylanlarimer2003@gmail.com>
2023-10-06 11:04:36 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+let submit = document.getElementById("submit");
+
+let addClass = ['container', 'col-sm', 'text-light'];
+
+submit.addEventListener("click", addToTable);
+
+function addToTable() {
+    let myTable = document.getElementById('myTable');
+    let fullName = document.getElementById("fullName").value;
+    let email = document.getElementById("email").value;
+    let phoneNumber = document.getElementById("phoneNumber").value;
+
+    let row = document.createElement('tr');
+    let dataName = document.createElement('td');
+    let dataEmail = document.createElement('td');
+    let dataNumber = document.createElement('td');
+
+    console.log(fullName, email, phoneNumber);
+
+    dataName.innerHTML = fullName;
+    dataName.classList.add(...addClass);
+
+    dataEmail.innerHTML = email;
+    dataEmail.classList.add(...addClass);
+
+    dataNumber.innerHTML = phoneNumber;
+    dataNumber.classList.add(...addClass);
+
+    row.classList.add(...['container', 'row']);
+
+    row.append(dataName, dataEmail, dataNumber);
+    myTable.append(row);
+
+    fullName = '';
+    console.log(fullName);
+}
